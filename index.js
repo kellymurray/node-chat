b15686622c1a556f9fbe633499a52153976cbc1b
Kelly Murray <kelly@theironyard.com>
2015-06-23 10:43:26 -0400

    initial commit to github

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+'use strict';
+
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+
+//Parse application/json requests properly
+app.use(bodyParser.json());
+
+//Serve all assets in the src folder
+app.use(express.static(__dirname + '/src'));
+
 // Routing, but this time on the server
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/things/:id', function (req, res) {
+  res.json({
+    yourId: req.params.id,
+    message: req.query.msg || 'Hello',
+    age: req.query.age || 'unknown'
+  });
+
+  //res.send('<h1>Hello, ' + req.params.id + '</h1>');
+  //res.json({ yourId: req.params.id, message: "Thanks for posting!" });
 });
 
 // Starting our router/applicatin listening on port 3000
